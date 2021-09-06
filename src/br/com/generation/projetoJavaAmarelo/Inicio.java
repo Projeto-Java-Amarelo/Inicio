542765cfbf02ff93355c8fa1f2a34eec56863dc7
abnerwerley <abnerwerley77@gmail.com>
2021-09-06 20:21:30 -0300

    apenas testando

---
+++ src/br/com/generation/projetoJavaAmarelo/Inicio.java
@@ -0,0 +1,10 @@
+package br.com.generation.projetoJavaAmarelo;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+
+		System.out.println("Olá,seja bem vindo(a), estamos no setembro amarelo, e ese projeto tem o objetivo de encurtar distâncias.");
+
+	}
+}
