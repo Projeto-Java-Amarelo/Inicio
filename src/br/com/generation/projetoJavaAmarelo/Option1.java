62a7a1e17fc57dc96c460e46784ee3e04fe3ebcd
Vitor <vitorvnc.f@gmail.com>
2021-09-12 20:58:44 -0300

    Funcoes basicas Option1 terminado.
    Versão com Option1 terminado

--- src/br/com/generation/projetoJavaAmarelo/Option1.java
+++ src/br/com/generation/projetoJavaAmarelo/Option1.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 
 public class Option1{
 	
+<<<<<<< Updated upstream
 	public static void ApresentaOp() { //vamos tentar colocar esse método num método "pai" (de uma superclasse) pra sobrecarrega-lo dentro das Op1, 2 e 3;
+=======
+
+	 
+
+public static void ApresentaOp() { //vamos tentar colocar esse método num método "pai" (de uma superclasse) pra sobrecarrega-lo dentro das Op1, 2 e 3;
+>>>>>>> Stashed changes
 	Scanner ler = new Scanner(System.in);
 	System.out.println("1 - TER O CARTÃO DO SUS: ");
 	System.out.println("Sempre que você usa os serviços do sistema único de saúde, precisará apresentar seu cartão/carteirinha");
