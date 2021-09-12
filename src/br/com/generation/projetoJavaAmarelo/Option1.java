f4a6250d6e7d376c6e3cc03fae93360bb36786b7
vitorvnc <vitorvnc.f@gmail.com>
2021-09-12 18:53:34 -0300

    Colocando Classe Option1
    Colocando a classe option1. Falta montar os ifs direito e ainda adicionar o resto dos textos.

---
+++ src/br/com/generation/projetoJavaAmarelo/Option1.java
@@ -0,0 +1,34 @@
+package br.com.generation.projetoJavaAmarelo;
+import java.util.Scanner;
+
+public class Option1{
+	
+	public static void ApresentaOp() { //vamos tentar colocar esse método num método "pai" (de uma superclasse) pra sobrecarrega-lo dentro das Op1, 2 e 3;
+	Scanner ler = new Scanner(System.in);
+	System.out.println("1 - TER O CARTÃO DO SUS: ");
+	System.out.println("Sempre que você usa os serviços do sistema único de saúde, precisará apresentar seu cartão/carteirinha");
+	System.out.println("Ele é gratuito e simples de conseguir: basta ir até uma unidade de saúde com o seu RG ou outro documento de identificação com foto.");
+	System.out.println("Após preencher um cadastro, sua carteirinha estará impressa e você já poderá ser atendido.");
+	
+	
+	
+	
+	ler.close();
+	
+
+
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	}
+}
